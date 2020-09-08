34e5588f60f53182942062e34908208532586ce8
Javito <fj.diazb@gmail.com>
2020-09-08 14:22:07 +0200

    cambio en clase

--- src/main/java/ejemplo/Jdb.java
+++ src/main/java/ejemplo/Jdb.java
@@ -1,5 +1,10 @@
 package ejemplo;
 
-public class Jdb {
+public class Jdb
+{
 
+	public int suma(int a, int b)
+	{
+		return a + b;
+	}
 }
--- src/main/java/ejemplo/Prueba.java
+++ src/main/java/ejemplo/Prueba.java
@@ -1,8 +1,10 @@
 package ejemplo;
 
-public class Prueba {
+public class Prueba
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 
 	}
